2fa3e1a2e327ee2445782dfaf6ba8155065fa8d2
WizardTechie <jez@wizard.technology>
2014-11-25 10:18:45 +0000

    adding google analytics

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -47,5 +47,20 @@
 </div><!--end container-->
 <?php wp_footer(); ?>
 <script type="text/javascript" async src="//assets.pinterest.com/js/pinit.js"></script>
+<?php
+if ( !is_user_logged_in() ) {
+	echo "<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-57072487-1', 'auto');
+  	  ga('send', 'pageview');
+
+	</script>";
+}
+
+?>
 </body>
 </html>
