6e51e8429cbc76818ffb951351295ce7054b165d
WizardTechie <jez@wizard.technology>
2014-11-30 16:43:13 +0000

    updating tracking now as .com

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -55,7 +55,7 @@ if ( !is_user_logged_in() ) {
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	  ga('create', 'UA-57072487-1', 'auto');
+	  ga('create', 'UA-57225641-1', 'auto');
   	  ga('send', 'pageview');
 
 	</script>";
